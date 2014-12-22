4e96e2fefe8aa0adff9648ae13584f64b8595732
jacobis <remind.jacob@gmail.com>
2014-12-22 19:21:55 +0900

    Add fabric commands

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+from fabric import colors
+from fabric.api import env, local, task
+
+env.run = 'python manage.py'
+
+DEFAULT_PORT = 8000
+DEFAULT_DJANGO_SETTINGS_MODULE = 'crawler.settings.local'
+
+@task
+def runserver(port=DEFAULT_PORT, settings=DEFAULT_DJANGO_SETTINGS_MODULE):
+    local('%s runserver_plus 0.0.0.0:%s --settings=%s' % (env.run, port, settings))
