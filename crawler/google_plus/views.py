766ee94466406ef982bb433e30b68c58df4ea2e7
jacobis <remind.jacob@gmail.com>
2014-12-23 12:52:02 +0900

    Added support for query based activity search

--- crawler/google_plus/views.py
+++ crawler/google_plus/views.py
@@ -10,26 +10,36 @@ from .models import ActivityObject
 
 
 def activity_list(request):
-    user_id = request.GET.get('user_id')
+    user_id = request.GET.get('user_id', None)
+    query = request.GET.get('query', None)
     collection = request.GET.get('collection', 'public')
+    language = request.GET.get('language', 'ko')
     key = request.GET.get('key')
 
-    url = 'people/%s/activities/%s' % (user_id, collection)
-    params = {'key': key, 'maxResults': 100}
-    
-    items = []
+    if user_id:
+        url = 'people/%s/activities/%s' % (user_id, collection)
+        params = {'maxResults': 100}
+    elif query and language:
+        url = 'activities'
+        params = {'query': query, 'maxResults': 20, 'language': language}
+
+    params.update({'key': key})
+
+    item_list = []
 
     while True:
         response = google_api(url, params)
         next_page_token = response.get('nextPageToken', None)
-        items += response.get('items')
+        items = response.get('items')
 
         if next_page_token:
             params.update({'pageToken': next_page_token})
+        if items:
+            item_list += items
         else:
             break
 
-    activity_objects = [ActivityObject(json="%s" % json.dumps(item)) for item in items]
+    activity_objects = [ActivityObject(json="%s" % json.dumps(item)) for item in item_list]
 
     if activity_objects:
         ActivityObject.objects.bulk_create(activity_objects)
