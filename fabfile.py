f7db35f6054757cdd19df57f557ce76700dff22f
jacobis <remind.jacob@gmail.com>
2014-12-27 22:44:36 +0900

    Added shell command

--- fabfile.py
+++ fabfile.py
@@ -11,3 +11,8 @@ DEFAULT_DJANGO_SETTINGS_MODULE = 'crawler.settings.local'
 @task
 def runserver(port=DEFAULT_PORT, settings=DEFAULT_DJANGO_SETTINGS_MODULE):
     local('%s runserver_plus 0.0.0.0:%s --settings=%s' % (env.run, port, settings))
+
+
+@task
+def shell(settings=DEFAULT_DJANGO_SETTINGS_MODULE):
+    local('%s shell_plus --settings=%s' % (env.run, settings))
