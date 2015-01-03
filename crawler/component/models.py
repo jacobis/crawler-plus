935bb5703c590150ca3f05f6e4d09b24c7652c37
jacobis <remind.jacob@gmail.com>
2015-01-03 14:49:15 +0900

    Created a KeyManager object

--- crawler/component/models.py
+++ crawler/component/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class KeyManager(models.Model):
+    GOOGLE_PLUS = 'GP'
+    SERVICE_CHOICES = (
+        (GOOGLE_PLUS, 'Google Plus'),
+    )
+    service = models.CharField(max_length=2, choices=SERVICE_CHOICES, default=GOOGLE_PLUS)
+    key = models.CharField(max_length=200)
+    note = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
